5e30a42daaaf7ef39e57c221899440626c3f8378
thesoulseizure <dineshchandra962@gmail.com>
2025-05-03 18:16:31 +0530

    Update BusBookingSystemApplication.java

--- src/main/java/com/busbooking/bus_booking_system/BusBookingSystemApplication.java
+++ src/main/java/com/busbooking/bus_booking_system/BusBookingSystemApplication.java
@@ -2,12 +2,15 @@ package com.busbooking.bus_booking_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.busbooking.bus_booking_system.repository")
+@EntityScan(basePackages = "com.busbooking.bus_booking_system.entity")
 public class BusBookingSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BusBookingSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BusBookingSystemApplication.class, args);
+    }
 }
