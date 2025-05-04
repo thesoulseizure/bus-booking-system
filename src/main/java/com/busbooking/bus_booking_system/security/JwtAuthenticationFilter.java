ebab9647f487fe0626c5bdebdf0e8a9229a93863
thesoulseizure <dineshchandra962@gmail.com>
2025-05-04 14:32:26 +0530

    Update JwtAuthenticationFilter.java

--- src/main/java/com/busbooking/bus_booking_system/security/JwtAuthenticationFilter.java
+++ src/main/java/com/busbooking/bus_booking_system/security/JwtAuthenticationFilter.java
@@ -1,3 +1,19 @@
+package com.busbooking.bus_booking_system.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
